4437e24db75b38e69b6b2f1a192ec56463013be4
sivsnkr <ced17i026@iiitdm.ac.in>
2021-05-19 06:59:52 +0530

    add multiple provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,22 @@ import 'package:provider/provider.dart';
 import './screens/product_overview_screen.dart';
 import './providers/products.dart';
 import './screens/product_details_screen.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'MyShop',
