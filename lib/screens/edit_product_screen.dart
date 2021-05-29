7bb8e223c5b87f109122ff48809249cf3a7dfe33
sivsnkr <ced17i026@iiitdm.ac.in>
2021-05-29 10:57:57 +0530

    update

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../providers/product.dart';
+
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
   @override
@@ -8,10 +10,24 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  var _editedProduct = new Product(
+    id: "",
+    description: "",
+    imageUrl: "",
+    price: -1,
+    title: "",
+    isFavourite: false,
+  );
   @override
   void dispose() {
     _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _saveForm() {
+    // logic goes here
+    _formKey.currentState?.save();
   }
 
   @override
@@ -19,26 +35,63 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                onSaved: (value) {
+                  _editedProduct = new Product(
+                    id: _editedProduct.id,
+                    description: _editedProduct.description,
+                    imageUrl: _editedProduct.imageUrl,
+                    price: _editedProduct.price,
+                    title: value.toString(),
+                    isFavourite: _editedProduct.isFavourite,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Price'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
+                onSaved: (value) {
+                  _editedProduct = new Product(
+                    id: _editedProduct.id,
+                    description: _editedProduct.description,
+                    imageUrl: _editedProduct.imageUrl,
+                    price: double.parse(value.toString()),
+                    title: _editedProduct.title,
+                    isFavourite: _editedProduct.isFavourite,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onSaved: (value) {
+                  _editedProduct = new Product(
+                    id: _editedProduct.id,
+                    description: value.toString(),
+                    imageUrl: _editedProduct.imageUrl,
+                    price: _editedProduct.price,
+                    title: _editedProduct.title,
+                    isFavourite: _editedProduct.isFavourite,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -68,6 +121,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       controller: _imageUrlController,
                       onEditingComplete: () {
                         setState(() {});
+                      },
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) {
+                        _editedProduct = new Product(
+                          id: _editedProduct.id,
+                          description: _editedProduct.description,
+                          imageUrl: value.toString(),
+                          price: _editedProduct.price,
+                          title: _editedProduct.title,
+                          isFavourite: _editedProduct.isFavourite,
+                        );
                       },
                     ),
                   ),
