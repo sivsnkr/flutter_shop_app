796ed415fd299bb9c2a83ad5587cfe1adfca553b
sivsnkr <ced17i026@iiitdm.ac.in>
2021-06-05 08:48:57 +0530

    add logic for ordering the product from cart - version 0

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 import './cart.dart';
 
@@ -25,38 +26,42 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> orderItem, double amount) {
+  Future<void> addOrder(List<CartItem> orderItem, double amount) async {
     final url = Uri.parse(
       'https://flutter-begineer-18e51-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',
     );
     final timeStamp = DateTime.now();
-    http.post(
-      url,
-      body: json.encode({
-        'amount': amount,
-        'products': orderItem
-            .map(
-              (item) => {
-                'id': item.id,
-                'title': item.title,
-                'price': item.price,
-                'quantity': item.quantity,
-              },
-            )
-            .toList(),
-        'dateTime': timeStamp.toIso8601String(),
-      }),
-    );
-    _orders.insert(
-      0,
-      OrderItem(
-        id: timeStamp.toString(),
-        amount: amount,
-        products: orderItem,
-        dateTime: timeStamp,
-      ),
-    );
-    notifyListeners();
+    try {
+      var response = await http.post(
+        url,
+        body: json.encode({
+          'amount': amount,
+          'products': orderItem
+              .map(
+                (item) => {
+                  'id': item.id,
+                  'title': item.title,
+                  'price': item.price,
+                  'quantity': item.quantity,
+                },
+              )
+              .toList(),
+          'dateTime': timeStamp.toIso8601String(),
+        }),
+      );
+      _orders.insert(
+        0,
+        OrderItem(
+          id: json.decode(response.body)['name'],
+          amount: amount,
+          products: orderItem,
+          dateTime: timeStamp,
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      throw HttpException("Unable to add order");
+    }
   }
 
   int count() {
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -6,78 +6,117 @@ import '../widgets/cart_item.dart';
 import '../providers/orders.dart';
 import './orders_screen.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   static const routeName = '/cart';
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
     final cartItems = cart.items.values.toList();
     final cartKeys = cart.items.keys.toList();
+
+    void placeOrder() async {
+      try {
+        setState(() {
+          _isLoading = true;
+        });
+        await Provider.of<Orders>(context, listen: false).addOrder(
+          cartItems,
+          cart.totalAmount,
+        );
+        cart.clearCart();
+        Navigator.of(context).pushNamed(OrdersScreen.routeName);
+      } catch (error) {
+        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Something went wrong"),
+          ),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Cart'),
       ),
-      body: Column(
-        children: [
-          Card(
-            margin: const EdgeInsets.all(15),
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: _isLoading
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text(
-                    'Total',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  // Spacer(),
-                  Chip(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    label: Text(
-                      'Rs ${cart.totalAmount.toStringAsFixed(2)}',
-                      style: Theme.of(context).primaryTextTheme.headline6,
-                    ),
-                  ),
-                  TextButton(
-                    child: const Text(
-                      'Order Now',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                    onPressed: () {
-                      if (cartItems.length > 0)
-                        Provider.of<Orders>(context, listen: false).addOrder(
-                          cartItems,
-                          cart.totalAmount,
-                        );
-                      cart.clearCart();
-                      Navigator.of(context).pushNamed(OrdersScreen.routeName);
-                    },
-                  ),
+                  CircularProgressIndicator(),
+                  Text(
+                    "We are adding your order",
+                    style: Theme.of(context).textTheme.headline6,
+                  )
                 ],
               ),
+            )
+          : Column(
+              children: [
+                Card(
+                  margin: const EdgeInsets.all(15),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Total',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        // Spacer(),
+                        Chip(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          label: Text(
+                            'Rs ${cart.totalAmount.toStringAsFixed(2)}',
+                            style: Theme.of(context).primaryTextTheme.headline6,
+                          ),
+                        ),
+                        TextButton(
+                          child: const Text(
+                            'Order Now',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onPressed: cartItems.length <= 0 ? null : placeOrder,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (ctx, index) {
+                      return CartItem(
+                        productId: cartKeys[index],
+                        id: cartItems[index].id,
+                        title: cartItems[index].title,
+                        price: cartItems[index].price,
+                        quantity: cartItems[index].quantity,
+                        removeItem: cart.removeItem,
+                      );
+                    },
+                    itemCount: cartItems.length,
+                  ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (ctx, index) {
-                return CartItem(
-                  productId: cartKeys[index],
-                  id: cartItems[index].id,
-                  title: cartItems[index].title,
-                  price: cartItems[index].price,
-                  quantity: cartItems[index].quantity,
-                  removeItem: cart.removeItem,
-                );
-              },
-              itemCount: cartItems.length,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
